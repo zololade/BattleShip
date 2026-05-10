96d71b6d400da1608857553e954eca2d4d2e8ea4
Ololade <73138104+zololade@users.noreply.github.com>
2026-05-10 14:09:45 +0100

    feat: initial project setup

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,7 +9,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
   eslintConfigPrettier,
