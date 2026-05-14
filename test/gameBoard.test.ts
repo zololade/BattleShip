3ad9ec9efaa6d30d1c76045b6cbc46ef177a3741
Ololade <73138104+zololade@users.noreply.github.com>
2026-05-15 00:17:10 +0100

    feat: add test for placeShip side effect

--- test/gameBoard.test.ts
+++ test/gameBoard.test.ts
@@ -2,11 +2,31 @@ import { describe, test, expect } from "vitest";
 import { GameBoard } from "../src/lib/GameBoard";
 
 describe("GameBoard class", () => {
-  test("should run", () => {
-    let newGame = new GameBoard();
+  let newGame = new GameBoard();
+
+  describe("placeShip side-effects", () => {
     newGame.placeShip();
 
-    console.log(newGame.occupied);
-    expect(true).toBe(true);
+    test("should check if occupied coordinate have 5 entries", () => {
+      expect(newGame.occupied.length === 5).toBe(true);
+    });
+
+    test("should check if no coordinate exist more than once", () => {
+      let flatOccupiedCoord = newGame.occupied
+        .flat()
+        .map((val) => `${val[0]}, ${val[1]}`);
+      let setOfOccupiedCoord = new Set(flatOccupiedCoord);
+      expect(flatOccupiedCoord.length === setOfOccupiedCoord.size).toBe(true);
+    });
+
+    test("should check if non of the vehicles have an empty coordinate", () => {
+      let checkVehicleLen = () => {
+        for (const [_vehicle, data] of Object.entries(newGame.vehicles)) {
+          if (data.coordinate.length <= 0) return false;
+        }
+        return true;
+      };
+      expect(checkVehicleLen()).toBe(true);
+    });
   });
 });
