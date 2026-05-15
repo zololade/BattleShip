af094de7b93116d75e04b004428a435a333e87ba
Ololade <73138104+zololade@users.noreply.github.com>
2026-05-15 02:27:06 +0100

    fix: remove skip from the ship test

--- test/ship.test.ts
+++ test/ship.test.ts
@@ -2,7 +2,7 @@ import { describe, test, expect } from "vitest";
 import { Ship } from "../src/lib/Ship";
 
 describe("Ship class", () => {
-  test.skip("should return true", () => {
+  test("should return true", () => {
     let testShip = new Ship(3);
     for (let i = 0; i < 3; i++) {
       testShip.hit();
