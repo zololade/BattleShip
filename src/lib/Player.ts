7390c88ebd67edaa38ec5011820813965162b797
Ololade <73138104+zololade@users.noreply.github.com>
2026-05-16 18:44:17 +0100

    feat: add player and computer class
    - add test for player and computer attack method

--- src/lib/Player.ts
+++ src/lib/Player.ts
@@ -1,1 +1,28 @@
-// import { GameBoard } from "./GameBoard";
+import { GameBoard } from "./GameBoard";
+
+export class Player {
+  protected opBoard: GameBoard;
+  public ownBoard = new GameBoard();
+  constructor(op: GameBoard) {
+    this.opBoard = op;
+  }
+
+  attack(x: number, y: number) {
+    this.opBoard.receiveAttack(x, y);
+  }
+}
+
+export class Computer extends Player {
+  constructor(op: GameBoard) {
+    super(op);
+  }
+
+  override attack() {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+
+    super.attack(x, y);
+  }
+}
+
+// there will be a driver that calls Comp.attack
--- test/player.test.ts
+++ test/player.test.ts
@@ -1,7 +1,26 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
+import { GameBoard } from "../src/lib/GameBoard";
+import { Computer, Player } from "../src/lib/Player";
 
-describe("nothing", () => {
-  test("expect nothing to be true", () => {
-    expect(true).toBe(true);
+describe("Players", () => {
+  let testBoard: GameBoard;
+  beforeEach(() => {
+    testBoard = new GameBoard();
+  });
+
+  describe("Computer player", () => {
+    test("expect Computer to attack", () => {
+      let comp = new Computer(testBoard);
+      comp.attack();
+      expect(testBoard.missedAttack.length > 0).toBe(true);
+    });
+  });
+
+  describe("Human player", () => {
+    test("expect Computer to attack", () => {
+      let player = new Player(testBoard);
+      player.attack(2, 8);
+      expect(testBoard.missedAttack).toContainEqual([2, 8]);
+    });
   });
 });
