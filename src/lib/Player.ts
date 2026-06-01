7ec24734624a412324942ac988511c3de801cb45
Ololade <73138104+zololade@users.noreply.github.com>
2026-06-01 08:01:19 +0100

    fix(ai): change active hit check to some

--- src/lib/Player.ts
+++ src/lib/Player.ts
@@ -120,7 +120,7 @@ export class Computer extends Player {
         if (notValid) continue;
         if (
           activeHits.length > 0 &&
-          !activeHits.every((hit) => cells.includes(hit))
+          !activeHits.some((hit) => cells.includes(hit))
         )
           continue;
 
